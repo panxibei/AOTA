55077648ea292f25b01b7a33dcc4801924696b1a
panxibei <panxibei@yandex.com>
2019-01-10 12:54:38 +0800

    fix mysql setup

--- config/database.php
+++ config/database.php
@@ -50,6 +50,21 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
+        'mysql_ssl' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
             'options'   => array(
 			    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
