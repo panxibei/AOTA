0de724379ae2ab1ec681ef52971b7570fb48afc7
panxibei <panxibei@yandex.com>
2019-04-12 14:45:19 +0800

    AOTA

--- app/Http/Controllers/Smt/qcreportController.php
+++ app/Http/Controllers/Smt/qcreportController.php
@@ -179,7 +179,7 @@ class qcreportController extends Controller
 				->where('pinming', $pinming)
 				->where('gongxu', $gongxu)
 				->first();
-				dd($res);
+			
 			$dianmei = $res['diantai'] * $res['pinban']; 
 			
 			$res = Smt_pdreport::select('xianti', 'banci')
@@ -188,7 +188,7 @@ class qcreportController extends Controller
 				->where('pinming', $pinming)
 				->where('gongxu', $gongxu)
 				->first();
-			dd($res);
+			
 			// 生产日报中的机种生产日期，暂保留，无用（返回但没用上）
 			// $shengchanriqi = date('Y-m-d H:i:s', strtotime($res['created_at']));
 
