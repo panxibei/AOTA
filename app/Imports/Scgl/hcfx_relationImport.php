bdca2fe52709edfc026299fe2fdf760a41fe735b
panxibei <panxibei@yandex.com>
2019-05-05 16:40:51 +0800

    AOTA

--- app/Imports/Scgl/hcfx_relationImport.php
+++ app/Imports/Scgl/hcfx_relationImport.php
@@ -33,7 +33,7 @@ class hcfx_relationImport implements ToModel, WithHeadingRow
 			// 'shuliang' => $row[9],
 
 			// 'riqi' => $row['日期'],
-			'jizhongming' => $row['机种'],
+			'jizhongming' => substr($row['机种'], 0, 7),
 			'tuopanxinghao' => $row['托盘型号'],
 			'tai_per_tuo' => $row['台/托'] ?? 0,
 			// 'zongshu' => is_null($row['总数']) ? 0 : $row['总数'],
