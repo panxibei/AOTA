103a6ba79ed4270b54b1bd84991bd163c54680ae
panxibei <panxibei@yandex.com>
2019-05-07 10:36:08 +0800

    AOTA

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -21,9 +21,9 @@
 
 	<font color="#035C98" style="font-size:36px;"><strong>ALPSALPINE</strong></font><br>
 	<font color="#035C98" style="font-size:18px;">ALPS ALPINE GROUP</font><br><br>
-	<font color="#ed4014" style="font-size:128px;"><strong>404</strong></font>
-	<h3>很抱歉，您要访问的页面不存在！</h3>
-	<span>@{{ time }} 秒后返回上一页</span>
+	<font color="#ed4014" style="font-size:128px;"><strong>403</strong></font>
+	<h3>很抱歉，您没有访问该页面的权限！</h3>
+	<span>@{{ time }} 秒后返回上一页或关闭本页面</span>
 
 </div>
 
