64281bfdc3021f76a12aedc6d85631142c7d8751
panxibei <panxibei@yandex.com>
2019-06-27 10:20:06 +0800

    AOTA

--- resources/views/smt/qcreport.blade.php
+++ resources/views/smt/qcreport.blade.php
@@ -5,6 +5,23 @@ SMT(QC report) -
 @parent
 @endsection
 
+@section('my_style')
+<style type="text/css">
+/* 合并单元格样式 */
+.subCol>ul>li{
+      margin:0 -18px;
+      list-style:none;
+      text-Align: center;
+      padding: 9px;
+      border-bottom:1px solid #ccc;
+      overflow-x: hidden;
+}
+.subCol>ul>li:last-child{
+  border-bottom: none
+}
+</style>
+@endsection
+
 @section('my_js')
 <script type="text/javascript">
 </script>
@@ -268,6 +285,8 @@ SMT(QC report) -
 
 					<i-row :gutter="16">
 						<i-col span="24">
+							<i-table ref="table2" height="200" size="small" border :columns="tablecolumns2" :data="tabledata2"></i-table>
+							<br><br>
 							<i-table ref="table1" height="460" size="small" border :columns="tablecolumns1" :data="tabledata1" @on-selection-change="selection => onselectchange1(selection)"></i-table>
 							<br><Page :current="pagecurrent" :total="pagetotal" :page-size="pagepagesize" @on-change="currentpage => oncurrentpagechange(currentpage)" show-total show-elevator></Page><br><br>
 						</i-col>
@@ -1040,7 +1059,160 @@ var vm_app = new Vue({
 		tabledata1: [],
 		tableselect1: [],
 		
+// ------------------------------------------------------------------
+tablecolumns2:[
+	    {title:'年级',key:'grade_name',align:'center'},
+        {title:'班级',key:'class_name',align:'center'},
+        {title:'日期',key:'date',align:'center'},
+        {title:'姓名',key:'student_name',align:'center'},
+        {
+            title: '考勤时段',
+            key: 'list',
+            align:'center',
+            render: (h, params) => {
+				return h('div', {
+					attrs: {
+						class:'subCol'
+					},
+				}, [
+				h('ul', params.row.list.map(item => {
+                    return h('li', {
+                    }, item.time_period_name)
+			    }))
+			]);
+            }
+		},
+		{
+            title: '正常',
+            key: 'list',
+            align:'center',
+            render: (h, params) => {
+              if(params.row.list[0].normal_amount!=undefined){
+                return h('div', {
+                  attrs: {
+                      class:'subCol'
+                  },
+                }, [
+                h('ul', params.row.list.map(item => {
+                    return h('li', {
+                    }, item.normal_amount)
+                }))
+              ]);
+              }else{
+                return h('div', [
+                  h('span', '----'),
+                ])
+              }
+            }
+        },
+        {
+            title: '迟到',
+            key: 'list',
+            align:'center',
+            render: (h, params) => {
+              if(params.row.list[0].be_late_amount!=undefined){
+                return h('div', {
+                  attrs: {
+                      class:'subCol'
+                  },
+                }, [
+                h('ul', params.row.list.map(item => {
+                    return h('li', {
+                    }, item.be_late_amount)
+                }))
+              ]);
+            }else{
+              return h('div', [
+                h('span', '----'),
+              ])
+            }
+          }
+        },
+        {
+            title: '早退',
+            key: 'list',
+            align:'center',
+            render: (h, params) => {
+              if(params.row.list[0].leave_early_amount!=undefined){
+                  return h('div', {
+                    attrs: {
+                        class:'subCol'
+                    },
+                  }, [
+                  h('ul', params.row.list.map(item => {
+                      return h('li', {
+                      }, item.leave_early_amount)
+                  }))
+                ]);
+              }else{
+                return h('div', [
+                  h('span', '----'),
+                ])
+              }
+            }
+        },
+        {
+            title: '未考勤',
+            key: 'list',
+            align:'center',
+            render: (h, params) => {
+              if(params.row.list[0].not_attendance_amount!=undefined){
+                return h('div', {
+                  attrs: {
+                      class:'subCol'
+                  },
+                }, [
+                h('ul', params.row.list.map(item => {
+                    return h('li', {
+                    }, item.not_attendance_amount)
+                }))
+              ]);
+              }else{
+                return h('div', [
+                  h('span', '----'),
+                ])
+              }
+            }
+        },
 
+      ],
+tabledata2: [{
+	list: [
+		{
+			"time_period_name": "上午上学",
+			"normal_amount": 0,
+			"be_late_amount": 1,
+			"leave_early_amount": 0,
+			"not_attendance_amount": 0
+		},
+		{
+			"time_period_name": "下午上学",
+			"normal_amount": 0,
+			"be_late_amount": 0,
+			"leave_early_amount": 0,
+			"not_attendance_amount": 1
+		},
+		{
+			"time_period_name": "下午放学",
+			"normal_amount": 0,
+			"be_late_amount": 0,
+			"leave_early_amount": 0,
+			"not_attendance_amount": 1
+		},
+		{
+			"time_period_name": "上午放学",
+			"normal_amount": 0,
+			"be_late_amount": 0,
+			"leave_early_amount": 1,
+			"not_attendance_amount": 0
+		}
+		],
+		grade_name: "幼儿园托儿班",
+		class_name: "幼儿园托儿班2班",
+		date: "2019-02-14",
+		student_name: "刘小明",
+	}],
+// ----------------------------------------------------
 		
 		// 日期范围过滤
 		qcdate_filter: [], //new Date(),
