33aa104499ad5845e40114363a0ac5e728d4fa2c
panxibei <panxibei@yandex.com>
2019-09-24 10:43:55 +0800

    AOTA

--- app/Http/Middleware/JwtAuth.php
+++ app/Http/Middleware/JwtAuth.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Models\Admin\Config;
+use App\Models\Admin\User;
+use Cookie;
 
 class JwtAuth
 {
@@ -26,8 +28,8 @@ class JwtAuth
 		$app_key = substr(config('app.key'), 19, 12);
 		$site_key = $config['SITE_KEY'];
 		if ($app_key != $site_key) die();
-		
-		
+
+
 		// 获取JSON格式的jwt-auth用户响应
 		$me = response()->json(auth()->user());
 		
@@ -36,10 +38,30 @@ class JwtAuth
 		// 用户信息：$user['id']、$user['name'] 等
 
 		// 判断数组为空，以此来判断是否有有效用户登录
-		if (! sizeof($user)) {
+		// 通过判断IP决定是否更换了登录地点
+		if (! sizeof($user) || $user['login_ip'] != $_SERVER['REMOTE_ADDR']) {
 			// 无有效用户登录，则认证失败，退回登录界面
 			// dd('credentials are invalid');
 
+			// if (! empty($user['id'])) {
+				// try	{
+				// 	User::where('id', $user['id'])
+				// 	->update([
+				// 		'login_ttl'	=> 0
+				// 	]);
+				// }
+				// catch (Exception $e) {
+				// 	// echo 'Message: ' .$e->getMessage();
+				// }
+			
+				auth()->logout();
+			
+			// }
+	
+		
+			// 删除cookie
+			Cookie::queue(Cookie::forget('token'));
+	
 			if($request->ajax()){
 				// 如果是ajax请求，则返回空数组，由axios处理返回登录页面
 				// return response()->json();
